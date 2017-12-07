821e76823e6f592714e25cc58f01c30e551b8bf9
Ken Richards <kenrichards84@gmail.com>
2017-12-06 21:05:38 -0700

    2 tasks per socket for ring.sh

--- Examples/ring.sh
+++ Examples/ring.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=1            # Number of cores per MPI rank
 #SBATCH --nodes=4                    # Number of nodes
 #SBATCH --ntasks-per-node=2          # How many tasks on each node
-#SBATCH --ntasks-per-socket=1        # How many tasks on each CPU or socket
+#SBATCH --ntasks-per-socket=2        # How many tasks on each CPU or socket
 #SBATCH --distribution=cyclic:cyclic # Distribute tasks cyclically on nodes and sockets
 #SBATCH --mem-per-cpu=100mb          # Memory per processor
 #SBATCH --time=00:05:00              # Time limit hrs:min:sec
