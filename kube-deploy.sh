1ee2a1e0a686bc8b93728dae65a1cebfca3140aa
Will <iq20064@bristol.ac.uk>
2023-06-27 11:21:47 +0100

    Converted conf files to manifests

--- kube-deploy.sh
+++
@@ -1,3 +0,0 @@
-kubectl apply -f 'kube-manifests/*.yaml'
-kubectl create configmap slurm-conf-configmap --from-file=slurm.conf
-kubectl create configmap slurmdbd-conf-configmap --from-file=slurmdbd.conf
